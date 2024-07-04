6eb15742b284783a1e725230d2e6c3f872a583bb
Muhammad Umar Malik <89419975+MuhammadUmarMalik@users.noreply.github.com>
2024-07-05 01:43:08 +0500

    create admin page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ import BecomeSeller from "./screens/SellerScreens/BecomeSeller";
 import SellerProfile from "./screens/SellerScreens/SellerProfile";
 import ListABook from "./screens/SellerScreens/ListABook";
 import OrderList from "./screens/SellerScreens/OrderList";
-
+import AdminPage from "./screens/admin/Admin";
 
 const App = () => {
   return (
@@ -32,19 +32,29 @@ const App = () => {
           <Route path="about" element={<About />} />
           <Route path="books" element={<Books />} />
           <Route path="tutor" element={<Tutor />} />
-          <Route path="/header/category" element={<Category/>}/>
+          <Route path="/header/category" element={<Category />} />
           <Route path="category/:categoryId" element={<CategoryDetails />} />
           <Route path="book/:subcategoryId" element={<BookDetails />} />
-          <Route path="/header/offer/:subcategoryId" element={<OfferBookPrice/>} />
-          <Route path="/header/offer/:subcategoryId" element={<OfferBookPrice/>}  />
-          <Route path="/header/customer-profile" element={<CustomerProfile/>} />
-          <Route path="/header/order-history" element={<OrderHistory/>} />
-          <Route path="/header/listed-books" element={<ViewListedBooks/>} />
-          <Route path="/header/become-seller" element={<BecomeSeller/>} />
-          <Route path="/header/seller-profile" element={<SellerProfile/>} />
-          <Route path="/header/list-a-book" element={<ListABook/>} />
-          <Route path="/header/order-list" element={<OrderList/>} />
+          <Route
+            path="/header/offer/:subcategoryId"
+            element={<OfferBookPrice />}
+          />
+          <Route
+            path="/header/offer/:subcategoryId"
+            element={<OfferBookPrice />}
+          />
+          <Route
+            path="/header/customer-profile"
+            element={<CustomerProfile />}
+          />
+          <Route path="/header/order-history" element={<OrderHistory />} />
+          <Route path="/header/listed-books" element={<ViewListedBooks />} />
+          <Route path="/header/become-seller" element={<BecomeSeller />} />
+          <Route path="/header/seller-profile" element={<SellerProfile />} />
+          <Route path="/header/list-a-book" element={<ListABook />} />
+          <Route path="/header/order-list" element={<OrderList />} />
           <Route path="contact" element={<Contact />} />
+          <Route path="admin" element={<AdminPage />} />
         </Route>
       </Routes>
     </Router>
